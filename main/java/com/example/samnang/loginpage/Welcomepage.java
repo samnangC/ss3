b5115a7625fd5863d32d21fe64a684358706b45a
Samnang <samnang.n@ligercambodia.org>
2017-11-20 17:26:32 +0700

    first commit

---
+++ main/java/com/example/samnang/loginpage/Welcomepage.java
@@ -0,0 +1,13 @@
+package com.example.samnang.loginpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcomepage);
+    }
+}
